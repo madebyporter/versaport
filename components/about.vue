8f1ab13d000b571505cc7f1194ecefaff6e3fff3
madebyporter <general@madebyporter.com>
2023-10-15 17:00:01 -0400

    remove link to submit from about

--- components/about.vue
+++ components/about.vue
@@ -30,7 +30,6 @@
           <p>I'm Chris Porter, the founder of <a href="https://www.madebyporter.com" target="_blank">Made by Porter</a>. I started as a freelance design engineer in 2004, working on various projects, from design to development. Over time, I've focused on software for productivity and e-commerce.</p>
           <p>My goal is to facilitate collaboration between designers and developers. I've curated a list of design engineers who, like me, value the synergy of small teams and a deep understanding of both design and development.</p>
           <p>This web app is a database of design engineers, where you can discover potential collaborators, partners, or mentors. In the future, you'll be able to add yourself directly, but for now, I review and add profiles personally.</p>
-          <p>If you'd like to be included or make changes, <button @click="showAddPersonForm">click here</button>. I'll review your work and make the necessary updates.</p>
           <p>For edits or removal, simply send me an <a href="mailto:general@madebyporter.com?subject=ThinkVersa%20Design Engineer%20Edit/Removal">email</a> with your name and your requests.</p>
         </article>
       </div>
