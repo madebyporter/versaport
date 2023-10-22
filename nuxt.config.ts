2f7bdcdbfd9ec67e50b20eadad17ff14861756f0
madebyporter <general@madebyporter.com>
2023-10-22 16:42:16 -0400

    added meta fields

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,22 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+      ],
+      meta: [
+        { name: 'msapplication-TileColor', content: '#da532c' },
+        { name: 'theme-color', content: '#ffffff' },
+        { hid: 'description', name: 'description', content: 'Find Design Engineers To Scale Quickly.' },
+        { hid: 'og:title', property: 'og:title', content: 'Think Versa' },
+        { hid: 'og:description', property: 'og:description', content: 'Find Design Engineers To Scale Quickly.' },
+        { hid: 'og:image', property: 'og:image', content: '/og-image.jpg' },
+        { hid: 'twitter:card', name: 'twitter:card', content: '/og-image.jpg' },
+      ],
       script: [
         {
           // Font Awesome
