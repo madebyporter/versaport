0a31f985a6907100add66807a3e97dcd17149de3
madebyporter <general@madebyporter.com>
2023-10-28 14:41:19 -0400

    split up success and error message. Clear form on submit

--- components/addPerson.vue
+++ components/addPerson.vue
@@ -8,9 +8,21 @@
   var base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base('appKgpEdY2KYZ7WGj');
 
   const emit = defineEmits();
+  const isSubmitting = ref(false);
+  const submitStatus = ref('');
+  const modalCount = ref(0); // Initialize modal count
+  const formSubmitted = ref(false);
+  const successMessage = ref('');
+  const errorMessage = ref('');
+  const formData = ref({
+    Name: '',
+    Email: '',
+    Portfolio: '',
+    Marketing: false,
+    Message: '',
+  });
 
   // Disable body scroll when mounted and enable when unmounted
-  const modalCount = ref(0); // Initialize modal count
   onMounted(() => {
     // Increase modal count when this modal is opened
     modalCount.value++;
@@ -26,17 +38,6 @@
       document.body.classList.remove('overflow-hidden'); // Remove class when all modals are closed
     }
   });
-  console.log(modalCount.value)
-  const isSubmitting = ref(false);
-  const submitStatus = ref('');
-
-  const formData = ref({
-    Name: '',
-    Email: '',
-    Portfolio: '',
-    Marketing: false,
-    Message: '',
-  });
 
   function closeModal() {
     emit('close');
@@ -44,11 +45,10 @@
 
   async function submitForm() {
     isSubmitting.value = true;
-
     // Name validation
-    const nameParts = formData.value.Name.split(' ');
-    if (nameParts.length !== 2 || nameParts[0].length < 2 || nameParts[1].length < 2) {
-      submitStatus.value = 'Invalid name! Make sure you have a first and last name, each with more than one character.';
+    const name = formData.value.Name;
+    if (!name) {
+      errorMessage.value = 'Name cannot be empty!';
       isSubmitting.value = false;
       return;
     }
@@ -56,7 +56,7 @@
     // Email validation
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (!emailPattern.test(formData.value.Email)) {
-      submitStatus.value = 'Invalid email format!';
+      errorMessage.value = 'Invalid email format!';
       isSubmitting.value = false;
       return;
     }
@@ -64,7 +64,7 @@
     // Portfolio URL validation
     const urlPattern = /^(ftp|http|https):\/\/[^ "]+$/;
     if (!urlPattern.test(formData.value.Portfolio)) {
-      submitStatus.value = 'Invalid portfolio URL!';
+      errorMessage.value = 'Invalid portfolio URL!';
       isSubmitting.value = false;
       return;
     }
@@ -82,10 +82,11 @@
         }
       ]);
       console.log('Airtable response:', response);
-      submitStatus.value = 'Received your submission. I will review your submission within the next 7 days and contact you if approved!';
+      successMessage.value = 'Received your submission. I will review your submission within the next 7 days and contact you if approved!';
+      formSubmitted.value = true;
     } catch (error) {
       console.log('Airtable error:', error);
-      submitStatus.value = 'Submission failed. Something in your form is wrong or the form is broken.';
+      errorMessage.value = 'Submission failed. Something in your form is wrong or the form is broken.';
     }
     isSubmitting.value = false;
   }
@@ -93,7 +94,7 @@
 
 <template>
   <div class="h-auto min-w-[260px] max-w-[560px] m-auto mb-10">
-    <form @submit.prevent="submitForm" class="flex flex-col w-full gap-2 mb-4">
+    <form v-if="!formSubmitted" @submit.prevent="submitForm" class="flex flex-col w-full gap-2 mb-4">
       <div class="mb-2">
         <h2 class="font-bold text-xl text-left">Submit a Design Engineer</h2>
         <p class="font-medium text-md text-left">Please submit you or a person you think is a great Design Engineer. This means a person that can design and code, as two specialties, done together.</p>
@@ -102,12 +103,16 @@
       <FormInput v-model="formData.Email" type="email" placeholder="chris@campbell.com" label="Email Address" />
       <FormInput v-model="formData.Portfolio" type="url" placeholder="https://chriscampbell.com" label="Portfolio Link" />
       <FormInput v-model="formData.Message" type="textarea" placeholder="Add your top 3 design and top 3 coding stacks, languages, software and industries here." label="Stack/Languages/Software" />
-      <FormInput v-model="formData.Marketing" type="checkbox" label="Subscribe to get future lists of the latest design engineers in your inbox." />
+      <FormInput v-model="formData.Marketing" type="checkbox" label="Subscribe to learn when new resources are launched!" />
+
+      <div v-if="errorMessage" class="text-red-600 text-lg font-medium">
+        {{ errorMessage }}
+      </div>
 
       <button type="submit" :disabled="isSubmitting" class="rounded-full bg-neutral-900 hover:bg-neutral-700 text-white px-5 py-2.5 uppercase">Submit</button>
     </form>
-    <div v-if="submitStatus">
-      {{ submitStatus }}
+    <div v-if="formSubmitted && successMessage" class="text-2xl font-medium">
+      {{ successMessage }}
     </div>
   </div>
 </template>
