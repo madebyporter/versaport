4fd07480e8e63a1e4a33800a503f9029869caf42
madebyporter <general@madebyporter.com>
2023-11-10 15:52:45 -0500

    added project name

--- components/carousel.vue
+++ components/carousel.vue
@@ -1,33 +1,22 @@
 <script setup lang="ts">
-  const props = defineProps<{
-    images: Array<{ src: string, alt: string, extraClasses: string }>,
-  }>()
+const props = defineProps<{
+  images: Array<{ src: string, alt: string, title: string, extraClasses: string }>
+}>()
 </script>
 
 <template>
   <div class="overflow-hidden w-full">
     <!-- Carousel Track -->
-    <div class="section-carousel-track flex flex-row items-center gap-20 px-10 overflow-x-scroll">
+    <div class="section-carousel-track flex flex-row items-end gap-20 px-10 overflow-x-scroll">
       <!-- Loop: Carousel Items -->
-      <div 
-        v-for="image in props.images" 
-        :key="image.src" 
-        :class="[
-          'section-work-item', 
-          'bg-neutral-200', 
-          'rounded-lg', 
-          'overflow-hidden',
-          'min-h-[300px]',
-          'md:min-h-[600px]',
-          image.extraClasses
-        ]"
-      >
-        <!-- Carousel Content -->
-        <img 
-          :src="image.src" 
-          :alt="image.alt"
-          class="w-full"
-        />
+      <div v-for="image in props.images" :key="image.src" class="flex flex-col justify-center gap-2">
+        <!-- Carousel Image -->
+        <div
+          :class="['bg-neutral-200', 'rounded-lg', 'overflow-hidden', 'min-h-[300px]', 'md:min-h-[600px]', image.extraClasses]">
+          <img :src="image.src" :alt="image.alt" class="w-full" />
+        </div>
+        <!-- Project Title -->
+        <div class="text-sm self-center px-5 py-2 text-neutral-500">{{ image.title }}</div>
       </div>
     </div>
   </div>
